77203eebabc4ed898305b1646bf968b21cbb99b4
Mejiao <Jonathan19991@gmail.com>
2022-02-21 14:45:33 -0500

    add an unimplemented class

---
+++ src/main/java/stockhelper/main/NaiveBalancer.java
@@ -0,0 +1,11 @@
+package stockhelper.main;
+
+import java.util.List;
+
+public class NaiveBalancer implements PortfolioBalancer {
+
+    @Override
+    public List<InvestmentLine> balancer(List<InvestmentLine> currentList, List<InvestmentLine> wishList) {
+        return null;
+    }
+}
