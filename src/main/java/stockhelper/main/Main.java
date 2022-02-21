479c06069c93a7fb866c7369c3a1fd72775ad429
Mejiao <Jonathan19991@gmail.com>
2022-02-21 13:55:01 -0500

    first classes

---
+++ src/main/java/stockhelper/main/Main.java
@@ -0,0 +1,7 @@
+package stockhelper.main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.err.println("Hello World");
+    }
+}
---
+++ src/test/java/stockhelper/main/MainTest.java
@@ -0,0 +1,12 @@
+package stockhelper.main;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+    @Test
+    public void boo() {
+        System.err.println("var");
+    }
+}
+
