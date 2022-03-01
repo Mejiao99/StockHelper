1ed88bfbd1c68b76105997b29713f16a64591dca
Mejiao <Jonathan19991@gmail.com>
2022-03-01 16:56:41 -0500

    PerAccountTransactionDecomposer implements

--- src/main/java/stockhelper/main/PerAccountTransactionDecomposer.java
+++ src/main/java/stockhelper/main/PerAccountTransactionDecomposer.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.ToString;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -15,39 +14,28 @@ import java.util.stream.Stream;
 public class PerAccountTransactionDecomposer {
     private SingleAccountTransactionDecomposer decomposer;
 
-    public List<Transaction> decompose(List<InvestmentLine> from, List<InvestmentLine> to) {
+    public List<Transaction> decompose(final List<InvestmentLine> from, final List<InvestmentLine> to) {
         final Set<String> allAccounts = Stream.concat(from.stream(), to.stream())
                 .map(InvestmentLine::getAccount)
                 .collect(Collectors.toSet());
 
-        List<Transaction> result = new ArrayList<>();
-        for (String account : allAccounts) {
+        final List<Transaction> result = new ArrayList<>();
+        for (final String account : allAccounts) {
+
             List<InvestmentLine> fromAccountTransactions = filterAccount(from, account);
             List<InvestmentLine> toAccountTransactions = filterAccount(to, account);
 
             List<Transaction> transactions = decomposer.decompose(fromAccountTransactions, toAccountTransactions);
+
             result.addAll(transactions);
         }
         return result;
     }
 
-    private List<InvestmentLine> filterAccount(List<InvestmentLine> investments, String account) {
-        List<InvestmentLine> accountList = new ArrayList<>();
-        for (InvestmentLine line : investments) {
-            if (line.getAccount().equals(account)) {
-                accountList.add(line);
-            }
-        }
-        return accountList;
+    private List<InvestmentLine> filterAccount(final List<InvestmentLine> investments, final String account) {
+        return investments.stream().
+                filter(investmentLine -> investmentLine.getAccount().equals(account)).
+                collect(Collectors.toList());
     }
-
-    private Set<String> getAllAccounts(List<InvestmentLine> listA) {
-        Set<String> accounts = new HashSet<>();
-        for (InvestmentLine line : listA) {
-            accounts.add(line.getAccount());
-        }
-        return accounts;
-    }
-
 
 }
