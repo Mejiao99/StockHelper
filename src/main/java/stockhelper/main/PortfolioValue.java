056a8391514bbeb97d2969fd97bf627076e4284e
Mejiao <Jonathan19991@gmail.com>
2022-03-01 18:00:01 -0500

    TransactionsExchangeCurrency add

--- src/main/java/stockhelper/main/PortfolioValue.java
+++ src/main/java/stockhelper/main/PortfolioValue.java
@@ -1,7 +1,6 @@
 package stockhelper.main;
 
 import java.util.List;
-import java.util.Map;
 
 public interface PortfolioValue {
     List<InvestmentLine> valueCalculator(List<InvestmentLine> currentItems, String currency);
