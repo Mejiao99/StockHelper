77d355027bd897bd308a2933326c99ac82acfadf
Mejiao <Jonathan19991@gmail.com>
2022-03-02 12:34:13 -0500

    PortfolioValueCalculatorPerDate

--- src/main/java/stockhelper/main/PortfolioValueCalculatorPerDate.java
+++ src/main/java/stockhelper/main/PortfolioValueCalculatorPerDate.java
@@ -5,6 +5,6 @@ import java.util.List;
 import java.util.Map;
 
 public interface PortfolioValueCalculatorPerDate {
-    Map<Currency, LocalDate> calculate(final List<InvestmentLine> balancedInvestments, final List<Transaction> transactions,
+    Map<LocalDate, Currency> calculate(final List<InvestmentLine> balancedInvestments, final List<Transaction> transactions,
                                        final LocalDate startDate, final LocalDate endDate);
 }
